50dabc8ec9fbfc38409c634c277ba3c991e111c0
Truecmetanka <56233532+Truecmetanka@users.noreply.github.com>
2022-12-21 13:39:56 +0300

    29-30

---
+++ src/lab29_30/ListNode.java
@@ -0,0 +1,8 @@
+
+
+package lab29_30;
+
+public class ListNode {
+    protected ListNode next;
+    protected MenuItem value;
+}
---
+++ src/lab29_30/QueueNode.java
@@ -0,0 +1,8 @@
+
+package lab29_30;
+
+public class QueueNode {
+    protected QueueNode next;
+    protected QueueNode prev;
+    protected Order value;
+}
