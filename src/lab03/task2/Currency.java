61aa12d70aaf80a690e228de1ebc308a3ddccdd3
Truecmetanka <matrosovdan@mail.ru>
2022-12-21 15:24:07 +0300

    3

---
+++ src/lab03/task2/Currency.java
@@ -0,0 +1,6 @@
+package lab03.task2;
+
+public interface Currency {
+    String getName();
+    double getRate();
+}
---
+++ src/lab03/task2/Dollars.java
@@ -0,0 +1,15 @@
+package lab03.task2;
+
+public class Dollars implements Currency {
+    private final double rate = 60;
+
+    @Override
+    public String getName() {
+        return "USD";
+    }
+
+    @Override
+    public double getRate() {
+        return rate;
+    }
+}
